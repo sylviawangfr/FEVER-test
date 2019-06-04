45e98047edac4a88a75667306bcffd60f5929aa1
Sylvia Wang <sylvia.wang@Sylvias-MacBook-Pro.local>
2019-06-04 16:19:38 +0800

    add setup steps

--- setup.sh
+++ setup.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
+
+export DIR_TMP="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+export PYTHONPATH=$PYTHONPATH:$DIR_TMP/src
+export PYTHONPATH=$PYTHONPATH:$DIR_TMP/utest
+export PYTHONPATH=$PYTHONPATH:$DIR_TMP/dep_packages
+export PYTHONPATH=$PYTHONPATH:$DIR_TMP/dep_packages/DrQA
+
+echo PYTHONPATH=$PYTHONPATH
+
 #download spacy model for multiple languages
-python -m spacy download xx_ent_wiki_sm
+python -m spacy download en_core_web_sm
 
 #install elasticsearch if not exists

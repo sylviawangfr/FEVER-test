d1cc0ed0d0d846769dec95f799f05d579a3f528e
Sylvia Wang <sylvia.wang@Sylvias-MacBook-Pro.local>
2019-06-24 09:16:15 +0800

    debug

--- src/pipeline.py
+++ src/pipeline.py
@@ -9,15 +9,15 @@ def doc_retriv():
     # doc retrieve
     # time = get_current_time_str()
 
-    # print("doc retrieving for dev...")
-    # doc_retriv_dev_list = get_doc_ids_and_fever_score(
-    #     config.FEVER_DEV_JSONL,
-    #     config.RESULT_PATH / f"{time}/dev_doc_retrive.jsonl",
-    #     top_k=10,
-    #     eval=True,
-    #     log_file=config.LOG_PATH / f"{time}/dev_doc_retrieve.log")
-    # print("done doc retrieving for dev.")
-    #
+    print("doc retrieving for dev...")
+    doc_retriv_dev_list = get_doc_ids_and_fever_score(
+        config.FEVER_DEV_JSONL,
+        config.RESULT_PATH / f"{time}/dev_doc_retrive.jsonl",
+        top_k=10,
+        eval=True,
+        log_file=config.LOG_PATH / f"{time}/dev_doc_retrieve.log")
+    print("done doc retrieving for dev.")
+
     # print("doc retrieving for train...")
     # doc_retriv_train_list = get_doc_ids_and_fever_score(
     #     config.FEVER_TRAIN_JSONL,
@@ -34,13 +34,13 @@ def doc_retriv():
     #                                              top_n=10,
     #                                              log_file=config.LOG_PATH / f"{time}/dev_ss_retrive_tfidf.log")
     # print("done with tfidf ss for dev")
-    print("tfidf ss retrieving for train...")
-    doc_retriv_train_list = str(config.RESULT_PATH / f"{time}/train_doc_retrive.jsonl")
-    tfidf_ss_train_list = tfidf_sentense_selection(doc_retriv_train_list,
-                                                   config.RESULT_PATH / f"{time}/train_ss_retrive_tfidf.jsonl",
-                                                   top_n=10,
-                                                   log_file=config.LOG_PATH / f"{time}/train_ss_retrive_tfidf.log")
-    print("done with tfidf ss for train")
+    # print("tfidf ss retrieving for train...")
+    # doc_retriv_train_list = str(config.RESULT_PATH / f"{time}/train_doc_retrive.jsonl")
+    # tfidf_ss_train_list = tfidf_sentense_selection(doc_retriv_train_list,
+    #                                                config.RESULT_PATH / f"{time}/train_ss_retrive_tfidf.jsonl",
+    #                                                top_n=10,
+    #                                                log_file=config.LOG_PATH / f"{time}/train_ss_retrive_tfidf.log")
+    # print("done with tfidf ss for train")
 
 if __name__ == "__main__":
     doc_retriv()

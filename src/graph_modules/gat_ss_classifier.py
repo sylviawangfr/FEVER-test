0b80919f14492f5a7f253c4d42267970efdb64eb
Sylvia Wang <sylvia.wang@Sylvias-MacBook-Pro.local>
2020-08-11 20:41:41 +0800

    train 0.005

--- src/graph_modules/gat_ss_classifier.py
+++ src/graph_modules/gat_ss_classifier.py
@@ -282,12 +282,12 @@ def train():
 
 
 def concat_tmp_data():
-    data_train = read_json_rows(config.RESULT_PATH / "sample_ss_graph_10000.jsonl")
+    data_train = read_json_rows(config.RESULT_PATH / "sample_ss_graph_10000.jsonl")[0:5000]
     # data_train.extend(read_json_rows(config.RESULT_PATH / "sample_ss_graph_10180.jsonl"))
     # data_train.extend(read_json_rows(config.RESULT_PATH / "sample_ss_graph_20000.jsonl"))
     # data_train.extend(read_json_rows(config.RESULT_PATH / "sample_ss_graph_25000.jsonl"))
     # data_train.extend(read_json_rows(config.RESULT_PATH / "sample_ss_graph_26020.jsonl"))
-    data_dev = read_json_rows(config.RESULT_PATH / "sample_ss_graph.jsonl")
+    data_dev = read_json_rows(config.RESULT_PATH / "sample_ss_graph.jsonl")[0:100]
     print(f"train data len: {len(data_train)}; eval data len: {len(data_dev)}\n")
     return data_train, data_dev
 

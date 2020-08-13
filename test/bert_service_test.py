2fe56eaebad2ec87a62345a45d60a853d241fae0
Sylvia Wang <sylvia.wang@Sylvias-MacBook-Pro.local>
2020-08-13 21:27:14 +0800

    test bert time

--- test/bert_service_test.py
+++ test/bert_service_test.py
@@ -1,7 +1,9 @@
 import unittest
 from datetime import datetime
 from dbpedia_sampler import bert_similarity
-from tqdm import tqdm
+from graph_modules.dbpedia_ss_gat_sampler import DBpediaGATSampler
+from utils.file_loader import *
+
 
 class TestDB(unittest.TestCase):
 
@@ -9,9 +11,16 @@ class TestDB(unittest.TestCase):
         ps = ['Ashley Judd', 'Where the Heart Is', 'Stockard Channing', 'Joan Cusack',
                                'Natalie Portman', '2000', 'Sally Field', 'Keith David', 'Dylan Bruno', 'James Frain']
         start = datetime.now()
-        for i in tqdm(range(100)):
+        for i in tqdm(range(10)):
             bert_similarity.get_phrase_embedding(ps)
         print(f"embedding time: {(datetime.now() - start).seconds}")
+
+    def test_gat_sampler(self):
+        start = datetime.now()
+        data = read_json_rows(config.RESULT_PATH / "sample_ss_graph.jsonl")[0:10]
+        sample = DBpediaGATSampler(data)
+        print(len(sample.graph_instances))
+        print(f"sampling time: {(datetime.now() - start).seconds}")
 
 
 if __name__ == '__main__':

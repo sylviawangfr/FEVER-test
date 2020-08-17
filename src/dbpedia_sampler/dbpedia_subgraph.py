8f0dc979aea3ad177e2c4b94a5591fc91cb8d7bf
Sylvia Wang <sylvia.wang@Sylvias-MacBook-Pro.local>
2020-08-17 23:44:37 +0800

    60840

--- src/dbpedia_sampler/dbpedia_subgraph.py
+++ src/dbpedia_sampler/dbpedia_subgraph.py
@@ -42,7 +42,7 @@ def construct_subgraph(sentence, doc_title=''):
     # print(json.dumps(merged_result, indent=4))
     return merged_result
 
-@profile
+# @profile
 def construct_subgraph_for_claim(claim_text):
     not_linked_phrases_l, linked_phrases_l = dbpedia_triple_linker.link_sentence(claim_text, '')
     phrases = not_linked_phrases_l + [i['text'] for i in linked_phrases_l]

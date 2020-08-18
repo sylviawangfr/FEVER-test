b781d481826c40ab7cc7bccd719c9af1b5aa79a4
Sylvia Wang <sylvia.wang@Sylvias-MacBook-Pro.local>
2020-08-18 11:41:30 +0800

    test memory

--- src/dbpedia_sampler/dbpedia_subgraph.py
+++ src/dbpedia_sampler/dbpedia_subgraph.py
@@ -275,7 +275,7 @@ def test_claim():
     s9 = "The term normally refers to the annually published Michelin Red Guide , the oldest " \
          "European hotel and restaurant reference guide , which awards Michelin stars for excellence " \
          "to a select few establishments ."
-    for i in range(1):
+    for i in range(20):
         t = construct_subgraph_for_claim(cc1)
         s = construct_subgraph_for_candidate(t, s9, "")
         del t

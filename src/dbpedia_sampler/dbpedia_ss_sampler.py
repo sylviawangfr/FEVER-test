c10a53689325292ac49758e6d0ee2efa7bd3e43a
Sylvia Wang <sylvia.wang@Sylvias-MacBook-Pro.local>
2020-08-24 10:18:00 +0800

    fix xml bug

--- src/dbpedia_sampler/dbpedia_ss_sampler.py
+++ src/dbpedia_sampler/dbpedia_ss_sampler.py
@@ -207,8 +207,7 @@ if __name__ == '__main__':
     # tfidf_train_data = read_json_rows(config.RESULT_PATH / "train_s_tfidf_retrieve.jsonl")[93420:100000]
     # tfidf_train_data = read_json_rows(config.RESULT_PATH / "train_s_tfidf_retrieve.jsonl")[84840:90000]
     # # tfidf_train_data = read_json_rows(config.RESULT_PATH / "train_s_tfidf_retrieve.jsonl")[110210:120000]
-    tfidf_train_data = read_json_rows(config.RESULT_PATH / "train_s_tfidf_retrieve.jsonl")
-    tfidf_train_data = tfidf_train_data[120000:len(tfidf_train_data)]
+    tfidf_train_data = read_json_rows(config.RESULT_PATH / "train_s_tfidf_retrieve.jsonl")[120000:130000]
     tfidf_to_graph_sampler(tfidf_train_data)
     # # # print(globals())
     # print(json.dumps(globals(), indent=1))

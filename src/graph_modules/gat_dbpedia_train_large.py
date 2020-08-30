4435aed8a8629bcb7416b30d5c2ca2dd718a04a0
Sylvia Wang <sylvia.wang@Sylvias-MacBook-Pro.local>
2020-08-30 17:47:10 +0800

    test

--- src/graph_modules/gat_dbpedia_train_large.py
+++ src/graph_modules/gat_dbpedia_train_large.py
@@ -16,7 +16,7 @@ import gc
 def collate_convert_to_dgl(instance_and_labels):
     # paired_g, labels = map(list, zip(*instance_and_labels))
     converter = DBpediaGATSampleConverter()
-    dglgraph_pairs, labels = converter.convert_dbpedia_to_dgl(instance_and_labels, parallel=False, num_worker=6)
+    dglgraph_pairs, labels = converter.convert_dbpedia_to_dgl(instance_and_labels, parallel=True, num_worker=8)
     g1_l = [i['graph1'] for i in dglgraph_pairs]
     g2_l = [i['graph2'] for i in dglgraph_pairs]
     return dgl.batch(g1_l), dgl.batch(g2_l), torch.tensor(labels)
@@ -56,7 +56,7 @@ def train(paras:GAT_para):
         loss_func.to(device)
 
     train_data_loader = DataLoader(train_data, batch_size=paras.batch_size, shuffle=True, collate_fn=collate_convert_to_dgl,
-                                   num_workers=paras.data_num_workers, pin_memory=True, drop_last=True)
+                                   pin_memory=True, drop_last=True)
     model.train()
     epoch_losses = []
     for epoch in range(epoches):
@@ -105,7 +105,7 @@ def eval(model_or_path, dbpedia_data):
     model.eval()
     # Convert a list of tuples to two lists
     test_data_loader = DataLoader(dbpedia_data, batch_size=160, shuffle=True,
-                                  collate_fn=collate_convert_to_dgl, drop_last=True, num_workers=16)
+                                  collate_fn=collate_convert_to_dgl, drop_last=True)
     all_sampled_y_t = 0
     all_argmax_y_t = 0
     test_len = 0

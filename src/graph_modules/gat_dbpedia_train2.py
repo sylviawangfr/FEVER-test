2b237a9a6f4912645857bc69f2a7d2dd25d6fe6e
Sylvia Wang <sylvia.wang@Sylvias-MacBook-Pro.local>
2020-08-31 21:25:40 +0800

    test

--- src/graph_modules/gat_dbpedia_train2.py
+++ src/graph_modules/gat_dbpedia_train2.py
@@ -152,8 +152,8 @@ def test_load_model():
 
 
 def read_data_in_file_batch():
-    data_train = read_files_one_by_one(config.RESULT_PATH / "sample_ss_graph_train_test")
-    data_dev = read_files_one_by_one(config.RESULT_PATH / "sample_ss_graph_dev_test")
+    data_train = read_files_one_by_one(config.RESULT_PATH / "sample_ss_graph_train")
+    data_dev = read_files_one_by_one(config.RESULT_PATH / "sample_ss_graph_dev")
     # print(f"train data len: {len(data_train)}; eval data len: {len(data_dev)}\n")
     return data_train, data_dev
 
@@ -164,8 +164,8 @@ def train_and_eval():
     data_train, data_dev = read_data_in_file_batch()
     paras = GAT_para()
     paras.data = DBpediaGATSampler(data_train, parallel=True, num_worker=8)
-    paras.epoches = 40
-    paras.batch_size = 128
+    paras.epoches = 400
+    paras.batch_size = 64
     paras.data_num_workers = 8
     model = train(paras)
     print(f"train time: {datetime.now() - start}")

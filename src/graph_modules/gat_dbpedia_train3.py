26c01cc3cefdbdb4fcff0bb5569832ad6fb37a2b
Sylvia Wang <sylvia.wang@Sylvias-MacBook-Pro.local>
2020-09-01 16:30:44 +0800

    test

--- src/graph_modules/gat_dbpedia_train3.py
+++ src/graph_modules/gat_dbpedia_train3.py
@@ -83,7 +83,7 @@ def train(paras: GAT_para, args):
 
 
     train_sampler = torch.utils.data.distributed.DistributedSampler(trainset)
-    train_data_loader = DataLoader(trainset, batch_size=paras.batch_size, shuffle=True, collate_fn=collate_with_dgl,
+    train_data_loader = DataLoader(trainset, batch_size=paras.batch_size, collate_fn=collate_with_dgl,
                                    pin_memory=True, num_workers=paras.data_num_workers, drop_last=True, sampler=train_sampler)
 
     model.train()

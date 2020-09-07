174ad00a8c94a12f72f7d786ad33668cdeec3e49
Sylvia Wang <sylvia.wang@Sylvias-MacBook-Pro.local>
2020-09-07 10:48:53 +0800

    test load model

--- src/graph_modules/gat_dbpedia_train2.py
+++ src/graph_modules/gat_dbpedia_train2.py
@@ -269,8 +269,8 @@ if __name__ == '__main__':
     data_dev = read_files_one_by_one(config.RESULT_PATH / "sample_ss_graph_dev_test")
     model_path = config.SAVED_MODELS_PATH / 'gat_ss_0.0001_epoch400_65.856_66.430'
     # data = read_json_rows(config.RESULT_PATH / 'sample_ss_graph.jsonl')
-    pred_prob(model_path, data_dev)
-    # test_load_model()
+    # pred_prob(model_path, data_dev)
+    test_load_model()
     # train_and_eval()
     # concat_tmp_data()
     # test_data()

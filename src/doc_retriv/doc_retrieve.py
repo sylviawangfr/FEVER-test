1ef8084cb7f41dcdca18e04d029b847a910f1e46
Sylvia Wang <sylvia.wang@Sylvias-MacBook-Pro.local>
2020-09-18 10:23:50 +0800

    test

--- src/doc_retriv/doc_retrieve.py
+++ src/doc_retriv/doc_retrieve.py
@@ -110,8 +110,8 @@ def rerun_failed_items(full_retri_doc, failed_list, updated_file_name):
 
 
 if __name__ == '__main__':
-    # i = retrieve_docs("A German-American fashion model starred in The Host.")
-    # print(i)
+    i = retrieve_docs("Jayasudha is an actor that stars in Daag.")
+    print(i)
     # j = retrieve_docs("Trouble with the Curve")
     # print(j)
     # get_doc_ids_and_fever_score(config.LOG_PATH / "test.jsonl", config.RESULT_PATH / f"{get_current_time_str()}_train_doc_retrive.jsonl")
@@ -119,7 +119,7 @@ if __name__ == '__main__':
     #                             config.RESULT_PATH / f"{get_current_time_str()}_train_doc_retrive.jsonl", top_k=10)
     #
     # get_doc_ids_and_fever_score(config.FEVER_TRAIN_JSONL, config.DOC_RETRV_TRAIN)
-    get_doc_ids_and_fever_score(config.FEVER_DEV_JSONL, config.RESULT_PATH / f"doc_dev_{get_current_time_str()}.jsonl")
+    # get_doc_ids_and_fever_score(config.FEVER_DEV_JSONL, config.RESULT_PATH / f"doc_dev_{get_current_time_str()}.jsonl")
     # get_doc_ids_and_fever_score(config.FEVER_TEST_JSONL, config.DOC_RETRV_TEST, eval=False)
     # print(retrieve_docs("Brian Wilson was part of the Beach Boys."))
     # get_doc_ids_and_fever_score(config.FEVER_TEST_JSONL, config.DOC_RETRV_TEST / get_current_time_str())

f5351b592ccd8ae21a5a29b75442d0ca62475148
Sylvia Wang <sylvia.wang@Sylvias-MacBook-Pro.local>
2020-09-21 09:03:42 +0800

    ss bert selection

--- src/BERT_test/ss_eval.py
+++ src/BERT_test/ss_eval.py
@@ -309,21 +309,23 @@ if __name__ == "__main__":
     paras.BERT_model = config.PRO_ROOT / "saved_models/bert_finetuning/ss_ss_3s_full2019_07_17_04:00:55"
     paras.BERT_tokenizer = config.PRO_ROOT / "saved_models/bert_finetuning/ss_ss_3s_full2019_07_17_04:00:55"
 
-    paras.output_folder = "bert_dev_pred_ss_" + get_current_time_str()
-    paras.original_data = read_json_rows(config.FEVER_DEV_JSONL)
-    paras.upstream_data = read_json_rows(config.RESULT_PATH / "dev_s_tfidf_retrieve.jsonl")
-    paras.mode = 'dev'
-    paras.pred = False
-    paras.sample_n = 20
-    paras.post_filter_prob = 0.5
-    paras.top_n = [10, 5]
-    paras.prob_thresholds = 0.4
-    pred_ss_and_save(paras)
+    paras.output_folder = "bert_ss_dev_" + get_current_time_str()
+    # paras.original_data = read_json_rows(config.FEVER_DEV_JSONL)
+    # paras.upstream_data = read_json_rows(config.RESULT_PATH / "dev_s_tfidf_retrieve.jsonl")
+    # paras.mode = 'dev'
+    # paras.pred = False
+    # paras.sample_n = 20
+    # paras.post_filter_prob = 0.5
+    # paras.top_n = [10, 5]
+    # paras.prob_thresholds = 0.4
+    # pred_ss_and_save(paras)
     # eval_ss_and_save(paras)
 
-    # paras.original_data = read_json_rows(config.FEVER_DEV_JSONL)
-    # paras.upstream_data = read_json_rows(config.RESULT_PATH / 'dev_doc_retrieve.jsonl')
-    # paras.mode = 'dev'
-    # paras.pred = True
-    # pred_ss_and_save(paras)
+    paras.original_data = read_json_rows(config.FEVER_DEV_JSONL)
+    paras.upstream_data = read_json_rows(config.RESULT_PATH / 'doc_dev.jsonl')
+    paras.mode = 'dev'
+    paras.pred = True
+    paras.top_n = [20]
+    paras.prob_thresholds = 0.1
+    pred_ss_and_save(paras)
 

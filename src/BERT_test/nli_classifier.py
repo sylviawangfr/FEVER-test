1c2301add9bcf69f7b2a1698d06a5284b217ccca
Sylvia Wang <sylvia.wang@Sylvias-MacBook-Pro.local>
2020-12-21 10:31:23 +0800

    eval with old model

--- src/BERT_test/nli_classifier.py
+++ src/BERT_test/nli_classifier.py
@@ -280,6 +280,7 @@ def nli_finetuning(upstream_train_data, output_folder='fine_tunning', sampler=No
                     raise e
         loss_for_chart.append(epoch_loss)
     draw_loss_epoch_detailed(loss_for_chart, f"loss_{output_folder}_{learning_rate}")
+    print(f"get rank: {torch.distributed.get_rank()}")
     if local_rank == -1 or torch.distributed.get_rank() == 0:
         # Save a trained model, configuration and tokenizer
         model_to_save = model.module if hasattr(model, 'module') else model  # Only save the model it-self
@@ -299,11 +300,11 @@ def nli_finetuning(upstream_train_data, output_folder='fine_tunning', sampler=No
     # else:
         # model = BertForSequenceClassification.from_pretrained(bert_model, num_labels=num_labels)
     # model.to(device)
-    print(f"get rank: {torch.distributed.get_rank()}")
+
     if do_eval and (local_rank == -1 or torch.distributed.get_rank() == 0):
         paras = bert_para.PipelineParas()
         paras.original_data = read_json_rows(config.FEVER_DEV_JSONL)
-        paras.upstream_data = read_json_rows(config.RESULT_PATH / "dev_s_tfidf_retrieve.jsonl")[0:500]
+        paras.upstream_data = read_json_rows(config.RESULT_PATH / "dev_s_tfidf_retrieve.jsonl")[0:50]
         paras.pred = False
         paras.mode = 'eval'
         paras.BERT_model = output_dir
@@ -314,7 +315,7 @@ def nli_finetuning(upstream_train_data, output_folder='fine_tunning', sampler=No
 
 
 if __name__ == "__main__":
-    train_data = read_json_rows(config.RESULT_PATH / "tfidf/train_2019_06_15_15:48:58.jsonl")[0:500]
+    train_data = read_json_rows(config.RESULT_PATH / "tfidf/train_2019_06_15_15:48:58.jsonl")[0:50]
     # train_data = read_json_rows(config.RESULT_PATH / 'train_s_tfidf_retrieve.jsonl')
     # nli_finetuning(train_data, output_folder="nli_train_extended" + get_current_time_str(), sampler='nli_nn')
     # train_data = read_json_rows(config.RESULT_PATH / "bert_ss_dev_10/eval_data_ss_10_dev_0.1_top[10].jsonl")[0:1000]

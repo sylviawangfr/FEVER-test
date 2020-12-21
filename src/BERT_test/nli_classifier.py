be6794a7d6176f5c7d3f81f57da7f38feb062afe
Sylvia Wang <sylvia.wang@Sylvias-MacBook-Pro.local>
2020-12-21 10:45:57 +0800

    eval with old model

--- src/BERT_test/nli_classifier.py
+++ src/BERT_test/nli_classifier.py
@@ -315,7 +315,7 @@ def nli_finetuning(upstream_train_data, output_folder='fine_tunning', sampler=No
 
 
 if __name__ == "__main__":
-    train_data = read_json_rows(config.RESULT_PATH / "tfidf/train_2019_06_15_15:48:58.jsonl")[0:50]
+    train_data = read_json_rows(config.RESULT_PATH / "tfidf/train_2019_06_15_15:48:58.jsonl")[0:500]
     # train_data = read_json_rows(config.RESULT_PATH / 'train_s_tfidf_retrieve.jsonl')
     # nli_finetuning(train_data, output_folder="nli_train_extended" + get_current_time_str(), sampler='nli_nn')
     # train_data = read_json_rows(config.RESULT_PATH / "bert_ss_dev_10/eval_data_ss_10_dev_0.1_top[10].jsonl")[0:1000]

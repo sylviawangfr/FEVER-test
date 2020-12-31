7ebebdb84943d04b763520225c5dd15865f47ef6
Sylvia Wang <sylvia.wang@Sylvias-MacBook-Pro.local>
2020-12-31 15:38:19 +0800

    rerun

--- src/doc_retriv/doc_retrieve_extend.py
+++ src/doc_retriv/doc_retrieve_extend.py
@@ -604,7 +604,7 @@ def rerun_failed_graph(folder):
             claim_dict = construct_subgraph_for_claim(claim, bc)
             claim_dict.pop('embedding')
             i['claim_dict'] = claim_dict
-            print(json.dumps(i['claim_dict'].get('linked_phrases_l'), indent=2))
+            # print(json.dumps(i['claim_dict'].get('linked_phrases_l'), indent=2))
             candidate_docs_2 = prepare_candidate2_example(i)
             if len(candidate_docs_2) < 1:
                 print("failed claim:", i.get('id'))

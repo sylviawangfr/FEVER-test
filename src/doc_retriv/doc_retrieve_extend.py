40c24664e7e8b32359ebf15601f3eeb489dafe94
Sylvia Wang <sylvia.wang@Sylvias-MacBook-Pro.local>
2021-01-05 22:11:53 +0800

    redo es

--- src/doc_retriv/doc_retrieve_extend.py
+++ src/doc_retriv/doc_retrieve_extend.py
@@ -384,13 +384,13 @@ def do_testset_graph2(folder):
 
 
 def do_dev_set():
-    folder = config.RESULT_PATH / "extend_20210104"
+    folder = config.RESULT_PATH / "extend_20210105"
     # es_data = read_json_rows(folder / "es_doc_10.jsonl")
     # eval_doc_preds(es_data, 5, config.LOG_PATH / 'doc_eval_1231')
 
 
-    # original_data = read_json_rows(config.FEVER_DEV_JSONL)
-    # prepare_candidate_doc1(original_data, folder / "es_doc_10.jsonl", folder / "es_doc_10.log")
+    original_data = read_json_rows(config.FEVER_DEV_JSONL)
+    prepare_candidate_doc1(original_data, folder / "es_doc_10.jsonl", folder / "es_doc_10.log")
 
     # data = read_json_rows(config.FEVER_DEV_JSONL)[0:10000]
     # prepare_claim_graph(data, folder / "claim_graph_10000.jsonl", folder / "claim_graph_10000.log")
@@ -411,8 +411,8 @@ def do_dev_set():
     # assert(len(es_data) == len(original_data) and (len(ent_data) == len(original_data)))
     # prepare_candidate_docs(original_data, es_data, ent_data, folder / "candidate_docs.jsonl", folder / "candidate_docs.log")
     # rerun_failed_graph(folder)
-    error_data = read_json_rows(folder / 'candidate_docs.log')
-    redo_example_docs(error_data, folder / "redo09.log")
+    # error_data = read_json_rows(folder / 'candidate_docs.log')
+    # redo_example_docs(error_data, folder / "redo09.log")
 
 
 

a57fa243bd49dbcc2d4332b15295a682825eeb81
Sylvia Wang <sylvia.wang@Sylvias-MacBook-Pro.local>
2021-02-15 22:47:43 +0800

    hardset

--- src/doc_retriv/doc_retrieve_extend.py
+++ src/doc_retriv/doc_retrieve_extend.py
@@ -513,9 +513,9 @@ def run_dev_failed_docs():
 
 if __name__ == '__main__':
     # claim = "Henry VIII (TV serial) stars a stage actor who has yet to act in film or television."
-    # claim = "Turin's Juventus Stadium is the home arena for Juventus F.C."
-    # entities, nouns = get_ents_and_phrases(claim)
-    # candidate_docs_1 = search_and_merge4(entities, nouns)
+    claim = "Turin's Juventus Stadium is the home arena for Juventus F.C."
+    entities, nouns = get_ents_and_phrases(claim)
+    candidate_docs_1 = search_and_merge4(entities, nouns)
 
 
     # data = read_json_rows(config.RESULT_PATH /"hardset2021/dev_has_multi_doc_evidence.jsonl")

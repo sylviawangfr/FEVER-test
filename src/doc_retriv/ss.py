94447a77d6c8a19753979837680cdae9ab462222
sylviawangfr <newtyjun@hotmail.com>
2021-03-22 17:22:22 +0800

    ss train

--- src/doc_retriv/ss.py
+++ src/doc_retriv/ss.py
@@ -18,7 +18,7 @@ def prepare_candidate_sents2_bert_dev(original_data, data_with_candidate_docs, o
     # paras.BERT_model = config.PRO_ROOT / "saved_models/bert_finetuning/ss_ss_3s_full2019_07_17_04:00:55"
     # paras.BERT_tokenizer = config.PRO_ROOT / "saved_models/bert_finetuning/ss_ss_3s_full2019_07_17_04:00:55"
     paras.BERT_model = config.PRO_ROOT / "saved_models/bert_finetuning/ss_ss_202103_94.9"
-    paras.BERT_tokenizer = config.PRO_ROOT / "saved_models/bert_finetuning/ss_ss_202101_94.9"
+    paras.BERT_tokenizer = config.PRO_ROOT / "saved_models/bert_finetuning/ss_ss_202103_94.9"
     paras.output_folder = output_folder
     paras.original_data = original_data
     paras.upstream_data = data_with_candidate_docs

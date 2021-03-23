bd3fb51403f20eaa667791bee1a9bb8227e7b19a
sylviawangfr <newtyjun@hotmail.com>
2021-03-23 14:45:17 +0800

    94.2

--- src/doc_retriv/ss.py
+++ src/doc_retriv/ss.py
@@ -17,8 +17,8 @@ def prepare_candidate_sents2_bert_dev(original_data, data_with_candidate_docs, o
     paras.mode = 'eval'
     # paras.BERT_model = config.PRO_ROOT / "saved_models/bert_finetuning/ss_ss_3s_full2019_07_17_04:00:55"
     # paras.BERT_tokenizer = config.PRO_ROOT / "saved_models/bert_finetuning/ss_ss_3s_full2019_07_17_04:00:55"
-    paras.BERT_model = config.PRO_ROOT / "saved_models/bert_finetuning/ss_ss_202103_94.9"
-    paras.BERT_tokenizer = config.PRO_ROOT / "saved_models/bert_finetuning/ss_ss_202103_94.9"
+    paras.BERT_model = config.PRO_ROOT / "saved_models/bert_finetuning/ss_ss_202103_94.2"
+    paras.BERT_tokenizer = config.PRO_ROOT / "saved_models/bert_finetuning/ss_ss_202103_94.2"
     paras.output_folder = output_folder
     paras.original_data = original_data
     paras.upstream_data = data_with_candidate_docs

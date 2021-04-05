d507b7713f0d47d2911295660c08951aa63e463d
sylviawangfr <newtyjun@hotmail.com>
2021-04-05 17:01:19 +0800

    test

--- src/BERT_test/ss_eval.py
+++ src/BERT_test/ss_eval.py
@@ -348,7 +348,7 @@ if __name__ == "__main__":
     paras.mode = 'train'
     paras.BERT_model = config.PRO_ROOT / "saved_models/bert_finetuning/ss_ss_202103_94.9"
     paras.BERT_tokenizer = config.PRO_ROOT / "saved_models/bert_finetuning/ss_ss_202103_94.9"
-    paras.output_folder = config.RESULT_PATH / 'train_2021_test'
+    paras.output_folder = config.RESULT_PATH / 'train_2021'
     paras.original_data = read_json_rows(config.FEVER_TRAIN_JSONL)[:80000]
     paras.upstream_data = read_json_rows(config.RESULT_PATH / 'train_2021/es_doc_10.jsonl')[:80000]
     paras.sample_n = 10

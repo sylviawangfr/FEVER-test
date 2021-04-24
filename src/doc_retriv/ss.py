f8db6d57dd41f03b2c08696c4ccefc603349e0a9
sylviawangfr <newtyjun@hotmail.com>
2021-04-24 15:22:10 +0800

    ss train

--- src/doc_retriv/ss.py
+++ src/doc_retriv/ss.py
@@ -167,8 +167,8 @@ def prepare_evidence_set_for_bert_nli(data_origin, data_with_bert_s,
     with tqdm(total=len(data_origin), desc=f"generating nli candidate") as pbar:
         for idx, example in enumerate(data_origin):
 
-            if idx not in [812, 869, 908, 926, 1361, 1368, 1445, 1485]:
-                continue
+            # if idx not in [812, 869, 908, 926, 1361, 1368, 1445, 1485]:
+            #     continue
             # ["Soul_Food_-LRB-film-RRB-<SENT_LINE>0", 1.4724552631378174, 0.9771634340286255]
             bert_s, bert_sid2score = get_bert_sids(data_with_bert_s[idx]['scored_sentids'])
             triples = [Triple(t_dict) for t_dict in data_with_tri_s[idx]['triples']]
@@ -746,13 +746,13 @@ def extend_candidate_one_hop(claim_dict, candidate_sentences: List[str]):
 if __name__ == '__main__':
     folder = config.RESULT_PATH / "hardset2021"
     hardset_original = read_json_rows(folder / "dev_has_multi_doc_evidence.jsonl")
-    candidate_docs = read_json_rows(folder / "candidate_docs.jsonl")
-    prepare_candidate_sents2_bert_dev(hardset_original, candidate_docs, folder)
-
+    # candidate_docs = read_json_rows(folder / "candidate_docs.jsonl")
+    # prepare_candidate_sents2_bert_dev(hardset_original, candidate_docs, folder)
+    #
     graph_data = read_json_rows(folder / "claim_graph.jsonl")
-    resource2docs_data = read_json_rows(folder / "graph_resource_docs.jsonl")
-    prepare_candidate_sents3_from_triples(graph_data, resource2docs_data, folder / "tri_ss.jsonl", folder / "tri_ss.log")
-
+    # resource2docs_data = read_json_rows(folder / "graph_resource_docs.jsonl")
+    # prepare_candidate_sents3_from_triples(graph_data, resource2docs_data, folder / "tri_ss.jsonl", folder / "tri_ss.log")
+    #
     tri_ss_data = read_json_rows(folder / "tri_ss.jsonl")
     bert_ss_data = read_json_rows(folder / "bert_ss_0.4_10.jsonl")
 
